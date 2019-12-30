4f4b2fc2e053b1f59676ca773fd066a3a369bb5f
Pratheesh Eswaran <pratheesh@Pratheeshs-MacBook-Pro.local>
2019-12-30 17:17:38 +0530

    updated title and css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { faRedo } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css';
 let json = require('./data.json');
+
 
 class App extends Component {
   constructor() {
@@ -10,41 +13,53 @@ class App extends Component {
       currentArt: json[Math.abs(Math.floor(Math.random() * (0 - Math.floor(json.length))) + 0)]
     }
   }
-// onClickEvent= () => {
-// console.log("testing",json.length)
-// this.setState({loading:true})
-// this.getRandomArt();
+  // onClickEvent= () => {
+  // console.log("testing",json.length)
+  // this.setState({loading:true})
+  // this.getRandomArt();
 
-//   }
+  //   }
   componentWillMount() {
 
-      this.getRandomArt(); 
-    
-  }
-  componentDidMount() {
-    document.addEventListener('click', this.getArt);
-  }
+    this.getArt();
 
-  componentWillUnmount() {
-    document.removeEventListener('click', this.getArt);
   }
-  getArt=()=>{
+  // componentDidMount() {
+  //   document.addEventListener('click', this.getArt);
+  // }
+
+  // componentWillUnmount() {
+  //   document.removeEventListener('click', this.getArt);
+  // }
+  getArt = () => {
     let jsonLength = json.length;
-    let randomArt =Math.abs(Math.floor(Math.random() * (0 - Math.floor(jsonLength))) + 0); 
-    console.log('randomArt',randomArt)
-    this.setState({currentArt: json[randomArt],loading:false})
+    let randomArt = Math.abs(Math.floor(Math.random() * (0 - Math.floor(jsonLength))) + 0);
+    console.log('randomArt', randomArt)
+    this.setState({ currentArt: json[randomArt], loading: false })
     console.log('art', json[randomArt], randomArt)
   }
-  getRandomArt(){
-    setInterval(()=> {
-     this.getArt()
-      }, 10000);
+  // getRandomArt(){
+  //   setInterval(()=> {
+  //    this.getArt()
+  //     }, 10000);
 
-  }
+  // }
   render() {
     return (
-      <div class='wrapper'>
-        <img id='img' src={this.state.currentArt.image}></img>
+      <div className='container'>
+        <div className='wrapper'>
+          <img id='img' src={this.state.currentArt.image}></img>
+
+        </div>
+        <div className='details'>
+          <div className='sub-details'>
+          <h1>{this.state.currentArt.title}</h1>
+          <span className='sub-title'><p>{this.state.currentArt.title} | {this.state.currentArt.attribution}</p></span>
+          </div>
+          <div className='refresh-btn' onClick={this.getArt}>
+            <p><FontAwesomeIcon icon={faRedo} /></p>
+          </div>
+        </div>
       </div>
     )
   }
